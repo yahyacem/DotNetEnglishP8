e9f5db9412b80156ed20f98b2c267eed91e93217
ycem <yahya.cherkaouielmoqadem@hotmail.com>
2023-08-10 13:12:43 +0200

    Backup for database added.

--- CalifornianHealthMonolithic.Data/Program.cs
+++ CalifornianHealthMonolithic.Data/Program.cs
@@ -14,22 +14,4 @@ builder.Services.AddDbContext<CHDBContext>(options =>
 // Setup Identity
 builder.Services.ConfigureIdentity();
 
-// Add services to the container.
-
-// builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-// builder.Services.AddEndpointsApiExplorer();
-// builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
-
-// // Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
-//     app.UseSwagger();
-//     app.UseSwaggerUI();
-// }
-
-// app.UseHttpsRedirection();
-// app.MapControllers();
-// app.Run();
--- backup.sql
+++
@@ -1,1 +0,0 @@
-OCI runtime exec failed: exec failed: unable to start container process: exec: "/Users/yahya/Documents": stat /Users/yahya/Documents: no such file or directory: unknown
